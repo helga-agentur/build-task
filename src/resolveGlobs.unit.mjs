11695621f1dfbcc0a605c924eb230f3bfdf6af88
Felix Steiner <felix_steiner@bluewin.ch>
2023-01-18 16:05:46 +0100

    feat(*): add watch option for scripts and styles
    Because watching through CLI with chokidar is much too slow, restarting the whole node process on every change

---
+++ src/resolveGlobs.unit.mjs
@@ -0,0 +1,24 @@
+import test from 'ava';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import resolveGlobs from './resolveGlobs.mjs';
+
+const basePath = dirname(fileURLToPath(new URL(import.meta.url)));
+
+test('resolves globs', async(t) => {
+    const files = resolveGlobs([
+        join(basePath, '../test/src/js/main*'),
+    ]);
+    t.deepEqual(files, [
+        join(basePath, '../test/src/js/main.js'),
+        join(basePath, '../test/src/js/main2.js'),
+    ]);
+});
+
+test('prepends basePath', async(t) => {
+    const files = resolveGlobs(['../test/src/js/main*'], basePath);
+    t.deepEqual(files, [
+        join(basePath, '../test/src/js/main.js'),
+        join(basePath, '../test/src/js/main2.js'),
+    ]);
+});
